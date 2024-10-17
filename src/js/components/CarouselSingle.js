cc7f47aa9e78012dc488fb08b26e9f8defba3f15
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-10-17 08:35:59 +0000

    style improvements

--- src/js/components/CarouselSingle.js
+++ src/js/components/CarouselSingle.js
@@ -1,6 +1,6 @@
 export default function (src, name, active = false) {
   const div = document.createElement("div");
   div.classList.add("carousel-item", active && "active");
-  div.innerHTML = `<img src="${src}" class="d-block w-100" alt="${name}" />`;
+  div.innerHTML = `<img src="${src}" class="d-block w-100 rounded" alt="${name}" />`;
   return div;
 }
